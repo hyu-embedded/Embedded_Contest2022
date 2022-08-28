eb88c76a2c8012ad8a9d163c49d28ae454b77fd8
CloverCho <snsc7878@gmail.com>
2022-08-28 20:28:59 +0900

    update server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.end('index route /');
+var data = 1;
+
+router.get("/", function (req, res, next) {
+    res.send({'data': data});
+});
+
+router.post("/", (req, res, next) => {
+    data = req.body.data;
+    res.send(`Successfully get data: ${data}`);
 })
 
 module.exports = router;
