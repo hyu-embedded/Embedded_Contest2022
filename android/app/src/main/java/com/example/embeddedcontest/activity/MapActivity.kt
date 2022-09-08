fd723484298630c98f4d6dd93596a459887ca0ad
CloverCho <snsc7878@gmail.com>
2022-09-08 15:52:34 +0900

    Put Edittext and button

---
+++ android/app/src/main/java/com/example/embeddedcontest/activity/MapActivity.kt
@@ -0,0 +1,33 @@
+// https://dalgonakit.tistory.com/137
+package com.example.embeddedcontest.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.embeddedcontest.R
+
+
+class MapActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        var btn_run = findViewById<Button>(R.id.btn_run)
+        var editText = findViewById<EditText>(R.id.editTextNumberDecimal)
+
+
+        btn_run.setOnClickListener() {
+            // 클릭시 동작할 코드
+            val text = editText.text
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+    }
+
+
+}
