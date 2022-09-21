83d77a741ba6bdd7f93338b27ee2773f94657463
Yeongbin <89433893+yeongbin7@users.noreply.github.com>
2022-09-22 00:07:56 +0900

    Update waterlevel.ino

--- arduino/waterlevel/waterlevel/waterlevel.ino
+++ arduino/waterlevel/waterlevel/waterlevel.ino
@@ -1,14 +1,50 @@
-int water_pin = A0;
+#include <printf.h>
+#include <RF24_config.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+#include<SPI.h>
+#include<RF24.h>
+RF24 radio(7,8);
+
+
+
+int water_pin =A0;
+int waterlevel = 0;
+int threshold = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode( A0, INPUT );
+  // put your setup code b  here, to run once:
+  radio.begin();
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(0x76);
+  radio.openWritingPipe(0xF0F0F0F0E2LL);
+  radio.enableDynamicPayloads();
+  radio.powerUp();
 
+  // water sensor
+  Serial.begin(115200);
+  pinMode(A0, INPUT);
+
+
+  
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(A0));
-  delay(100);
+  waterlevel = analogRead(A0);
+
+  if (waterlevel > threshold) {
+    String result = String(waterlevel, DEC);
+    radio.write(result.c_str(), result.length());
+    Serial.println(result); 
+  }
+  
+  
+  //const char text[] = "Hello World! yeongbin~";
+  //radio.write(&text, sizeof(text));
+  //Serial.println(text);
+
+  
+  delay(10); 
+
 }
