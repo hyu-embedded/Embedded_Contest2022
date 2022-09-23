c5a6b1d39dc23159bce772ec4f33eb9dd6ff01cd
CloverCho <snsc7878@gmail.com>
2022-09-24 00:38:37 +0900

    Update server

--- server/routes/android.js
+++ server/routes/android.js
@@ -5,15 +5,29 @@ router.get('/', (req, res, next) => {
     console.log(`Android request info...\n`)
 
     res.json([
-        {id: 0, loc: 35, lat: 120, floor: 1, waterlevel: 24, status: 3},
-        {id: 1, loc: 35, lat: 121, floor: 2, waterlevel: 24, status: 3},
-        {id: 2, loc: 35, lat: 122, floor: 4, waterlevel: 42, status: 5},
-        {id: 3, loc: 35, lat: 123, floor: 3, waterlevel: 52, status: 3},
-        {id: 4, loc: 35, lat: 124, floor: 5, waterlevel: 30, status: 1}
+        {id: 0, loc: 35, lat: 120, floor: 1, waterlevel: 24, status: 3, isAssigned: false},
+        {id: 1, loc: 35, lat: 121, floor: 2, waterlevel: 24, status: 3, isAssigned: true},
+        {id: 2, loc: 35, lat: 122, floor: 4, waterlevel: 42, status: 5, isAssigned: false},
+        {id: 3, loc: 35, lat: 123, floor: 3, waterlevel: 52, status: 3, isAssigned: false},
+        {id: 4, loc: 35, lat: 124, floor: 5, waterlevel: 30, status: 1, isAssigned: true}
     ])
 
 })
 
+
+router.get('/login', (req, res, next) => {
+    console.log(``)
+    var id = req.body.id;
+    var password = req.body.password;
+    
+    // Do login
+    if (id == 'yunsang' && password == '1234') {
+        res.status(200).send('OK');
+    } else {
+        res.status(404).send('Not Found');
+    }
+
+})
 
 router.get('/search', (req, res, next) => {
     
