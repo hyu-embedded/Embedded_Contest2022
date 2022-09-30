1fc0502e971a647a2edf73c3059aeb457c2193a2
CloverCho <snsc7878@gmail.com>
2022-09-30 15:22:54 +0900

    Update server: rasp

--- server/routes/android.js
+++ server/routes/android.js
@@ -3,26 +3,36 @@ const router = express.Router();
 
 var testData = {
     'count': 2,
-    '0': {id: 0, lat: 37.421946, loc: -122.084573, floor: 1, waterlevel: 24, status: 5, isAssigned: false},
-    '1': {id: 1, lat: 37.422182, loc: -122.0826, floor: 2, waterlevel: 24, status: 3, isAssigned: true},
+    '0': {id: 0, lat: 37.556132, loc: 127.049934, floor: 1, waterlevel: 56, status: 2, isAssigned: false},
+    '1': {id: 1, lat: 37.556945, loc: 127.049108, floor: 2, waterlevel: 38, status: 1, isAssigned: false},
 }
+
+const validate_user = (id, password) => {
+
+
+}
+
+const send_login_result = () => {
+
+}
+
+const send_neighbor_info = (loc, lat, zoom) => {
+
+
+}
+
+const update_database = (id, status) => {
+    
+}
+
 
 
 router.get('/', (req, res, next) => {
     console.log(`Android request info...\n`)
 
-    // res.json([
-    //     {id: 0, loc: 35, lat: 120, floor: 1, waterlevel: 24, status: 3, isAssigned: false},
-    //     {id: 1, loc: 35, lat: 121, floor: 2, waterlevel: 24, status: 3, isAssigned: true},
-    //     {id: 2, loc: 35, lat: 122, floor: 4, waterlevel: 42, status: 5, isAssigned: false},
-    //     {id: 3, loc: 35, lat: 123, floor: 3, waterlevel: 52, status: 3, isAssigned: false},
-    //     {id: 4, loc: 35, lat: 124, floor: 5, waterlevel: 30, status: 1, isAssigned: true}
-    // ])
-    console.log(`Query: ${req.query.lat}`)
-
-    res.json(testData)
-
 })
+
+
 
 
 router.get('/login', (req, res, next) => {
@@ -42,26 +52,39 @@ router.get('/login', (req, res, next) => {
 
 router.get('/search', (req, res, next) => {
     
-    var id = req.body.id;
-    var pos = req.body.pos;
-    var distance = req.body.distance;
+    var lat = req.query.lat;
+    var loc = req.query.loc;
+    var zoom = req.query.zoom;
 
-    console.log(`Client${id} request searching...\n
-    position: position: loc=${pos['loc']}, lat=${pos['lat']}\n
-    distance: ${distance}`);
+    if (testData[0].waterlevel < 100) {
+        testData[0].status = 2
+        testData[0].waterlevel += Math.floor(Math.random() * 10)
+    } else {
+        if (testData[0].status == 2) {
+            testData[0].status = 3;
+        }
+    }
+
+    res.json(testData);
 
 });
-
 
 
 router.get('/update', (req, res, next) => {
     var id = req.query.id;
     
-    testData[`${id}`].status = -1;
+    testData[`${id}`].isAssigned = true;
     console.log(`testData: ${JSON.stringify(testData)}`)
     res.json(testData);
 });
 
+
+router.get('/done', (req, res, next) => {
+    var id = req.query.id;
+
+    testData[`${id}`].status = -1;
+    res.json(testData);
+})
 
 
 module.exports = router;
--- server/routes/rasp.js
+++ server/routes/rasp.js
@@ -1,61 +1,136 @@
 const express = require('express');
+const { Model } = require('mongoose');
 const router = express.Router();
 const Sensor = require("../models/Sensor");
-
-const rpiClient = require('../managers/rpiClient');
 
 // start
 var id = 0;
 
 var rpiClients = {};
 
-//result
-router.get('/', (req, res, next) => {
-    res.json({'id': 'yunsang'})
-})
-
-
-router.post('/start', async (req, res, next) => {
-    // var {pos, floor, num_of_devices} = req.body;
+const create_new_client = async (lat, loc, floor) => {
     
-    // id += 1;
-    // var client = new rpiClient(id=id, position=pos, num_of_devices=num_of_devices);
-    // rpiClients[id] = client.getState();
-    // res.send(client.printInfo());
+    // Get ID
+    var new_id = 1;
+    Sensor.find().sort("-id").limit(1).exec((err, val)=>{
+        if (err) {
+            console.log(err);
+        } else {
+           new_id = val[0].id + 1; 
+        }
+    })
+
+    const new_sensor = new Sensor({
+        id: new_id,
+        loc: lat,
+        lat: loc,
+        floor: floor,
+        waterlevel: 0,
+        status: 1,
+    });
+
+    return {
+        id: new_id,
+        sensor: new_sensor
+    };
+}
+
+const update_database = async (msg_type, id, target_sensor) => {
     
-    id += 1;
-    var pos = req.body.pos;
+    if (msg_type == 'new_sensor') {
+
+        try {
+            await target_sensor.save();
+            return true;
+    
+        } catch (err){
+            console.log(err);
+            return false;
+        }
+    } else if (msg_type == 'update') {
+        try {
+            const target_client = await Sensor.findOne({"id": id});
+            target_client.waterlevel = target_sensor.waterlevel;
+            await target_client.save();
+            return true;
+
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+    return false;
+}
+
+const send_client_id = async () => {
+    var id = -1;
+    Sensor.find().sort("-id").limit(1).exec((err, val)=>{
+        if (err) {
+            console.log(err);
+        } else {
+           id = val[0].id; 
+        }
+    })
+    return id;
+}
+
+const validate_data = async (id, waterlevel, diff_loc, diff_lat, alpha) => {
+    const target_client = await Sensor.findById(id)
+    const previous_waterlevel = target_client.waterlevel
+
+    if (Math.abs(target_client.waterlevel - waterlevel) > alpha * target_client.waterlevel) {
+        return false;
+    }
+
+    const neighbors = Sensor.find({
+        loc: {
+            $gte: target_client.loc - diff_loc,
+            $lt: target_client.loc + diff_loc
+        },
+        lat: {
+            $gte: target_client.lat - diff_lat,
+            $lt: target_client.lat + diff_lat
+        },
+    }).filter(Math.abs(target_client.waterlevel - waterlevel) > alpha * target_client.waterlevel);
+
+    if (neighbors.length > 0) {
+        return false;
+    }
+
+    return true;
+}
+
+router.get('/start', async (req, res, next) => {
+        
+    var lat = req.body.pos.lat;
+    var loc = req.body.pos.loc;
     var floor = req.body.floor;
-    var num_of_devices = req.body.num_of_devices;
+    var timestamp = req.body.timestamp;
     
     console.log(`New clients with id ${id}:\nposition: loc=${pos['loc']}, lat=${pos['lat']}\nfloor=${floor}\nnumber of devices: ${num_of_devices}`);
     
+    const new_client_info = create_new_client(lat, loc, floor);
+    const new_id = (await new_client_info).id;
+    const new_client = (await new_client_info).sensor;
 
-    const newSensor = new Sensor({
-        id: id,
-        loc: req.body.pos['loc'],
-        lat: req.body.pos['lat'],
-        floor: req.body.floor,
-        waterlevel: 0,
-        status: 0,
-    });
-
-    try {
-        const savedSensor = await newSensor.save();
-        //res.status(201).json(savedSensor);
-        console.log(savedSensor)
-        res.status(201).json({'id': id});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }    
-
+    if (update_database("new_sensor", new_id, new_client)) {
+        res.json({'id': send_client_id()})
+    } else {
+        res.json({'id': -1})
+    }
+    
 });
 
 // UPDATE
-router.post('/result', (req, res, next) => {
-    console.log(req.body);
-    res.send('Successfully get data');
+router.post('/update', (req, res, next) => {
+    //res.send('Successfully get data');
+    const target_id = req.body.id;
+    const target_waterlevel = req.body.waterlevel;
+    const target_sensor = Sensor.findOne({"id": target_id});
+
+    if (validate_data(target_id, target_waterlevel, 1.0, 1.0, 0.8)) {
+        update_database("update", target_id, target_sensor);
+    }
 });
 
 module.exports = router;
